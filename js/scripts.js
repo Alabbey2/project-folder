5c1d16659fb6b2873d7c00c2c2ab130ad8e7d392
Alabbey2 <47299377+Alabbey2@users.noreply.github.com>
2020-08-25 14:52:14 +0300

    new commit with solution 7

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,27 @@
-var a = 3;
-var b = 5;
-var c = a + b;
-console.log(c);
+
+document.addEventListener('DOMContentLoaded', startScript);
+
+function startScript() {
+
+    console.log('Hello World')
+
+    document.body.style.backgroundImage = 'url("img/bg.jpg")';
+
+    var new_element = document.createElement("div");
+    new_element.setAttribute("id", "title-wrapper");
+
+    new_element.setAttribute("style",  "top: 50%; width: 100%; text-align: center; font-family: impact; position: absolute; text-transform: uppercase");
+   
+
+    document.body.appendChild(new_element);
+
+    var heading_element = document.createElement ("h1");
+    heading_element.setAttribute("id", "title");
+    heading_element.innerText = "Work under Progress";
+
+    heading_element.setAttribute("style", "width: fit-content; margin: auto; background: black; color: white");
+
+    document.body.appendChild (heading_element);
+
+
+};
